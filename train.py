96580a9bfb8a7dc168b3163a253aa4966929b55b
Ryosuke Tsuchida <j3350pai@sit.ac.jp>
2024-12-17 15:13:43 +0900

    無駄なスペースの排除

--- train.py
+++ train.py
@@ -69,17 +69,24 @@ for k in range(n_epochs):
     loss_train_history.append(loss_train)
     print(f'train loss: {loss_train:.3f}', end=', ')
 
+    time_start = time.time()
     loss_test = models.test(model, dataloader_test, loss_fn)
+    time_end = time.time()
     loss_test_history.append(loss_test)
-    print(f'test loss: {loss_train:.3f}', end=', ')
+    print(f'test loss: {loss_test:.3f}({time_end-time_start:.1f}s)')
 
-    acc_train = models.test_accuracy(model, dataloader_train)
-    acc_train_history.append(acc_train)
-    print(f'train accuracy: {acc_train*100:.3f}%')
-
-    acc_test = models.test_accuracy(model, dataloader_test)
-    acc_test_history.append(acc_test)
-    print(f'test accuracy: {acc_test*100:.3f}%')
+    if (k+1) % 5 ==0:
+        time_start = time.time()
+        acc_train = models.test_accuracy(model, dataloader_train)
+        time_end = time.time
+        acc_train_history.append(acc_train)
+        print(f'    train accuracy: {acc_train*100:.3f}%({time_end-time_start:.1f}s)')
+        
+        time_start = time.time()
+        acc_test = models.test_accuracy(model, dataloader_test)
+        time_end = time.time()
+        acc_test_history.append(acc_test)
+        print(f'test accuracy: {acc_test*100:.3f}%({time_end-time_start:.1f}s)')
 
 
 plt.plot(acc_train_history, label='train')
