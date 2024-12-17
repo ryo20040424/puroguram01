3325702232ce971d18f9c8556d992c822233813b
Ryosuke Tsuchida <j3350pai@sit.ac.jp>
2024-12-17 16:38:44 +0900

    deviceを指定２

--- models.py
+++ models.py
@@ -21,12 +21,12 @@ class MyModel(nn.Module):
 def test_accuracy(model, dataloader,device='cpu'):
     n_corrects = 0
 
-    model.to(device)
+    model = model.to(device)
 
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch=image_batch.to(device)
+        label_batch=label_batch.to(device)
         with torch.no_grad():
            logits_batch = model(image_batch)
 
@@ -40,11 +40,11 @@ def test_accuracy(model, dataloader,device='cpu'):
 def train(model, dataloader, loss_fn, optimizer,device='cpu'):
     model. train()
 
-    model.to(device)
+    model=model.to(device)
 
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch=image_batch.to(device)
+        label_batch=label_batch.to(device)
         logits_batch = model(image_batch)
 
         loss = loss_fn(logits_batch, label_batch)
@@ -58,12 +58,12 @@ def train(model, dataloader, loss_fn, optimizer,device='cpu'):
 def test(model, dataloader, loss_fn,device='cpu'):
     loss_total = 0.0
 
-    model.to(device)
+    model=model.to(device)
 
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch=image_batch.to(device)
+        label_batch=label_batch.to(device)
         with torch.no_grad():
             logits_batch = model(image_batch)
 
